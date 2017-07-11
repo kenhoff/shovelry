24b18ce25435359d9edaee9da005e2457b9d331a
Ken Hoff <ken@hoff.tech>
2017-07-10 21:47:47 -0600

    initial commit

---
+++ shovelry.js
@@ -0,0 +1,35 @@
+var shovelry = function(options) {
+	for (var element of document.getElementsByTagName("em")) {
+		var replacementTag = document.createElement("em")
+		var delay = 0;
+		for (var char of element.textContent) {
+			var newElement = document.createElement("span");
+			console.log(char);
+			if (char == " ") {
+				newElement.innerHTML = "&nbsp;"
+			} else {
+				newElement.innerHTML = char;
+			}
+			newElement.style.animationDelay = delay + "s";
+			replacementTag.appendChild(newElement)
+			delay -= 0.1;
+		}
+		element.innerHTML = replacementTag.innerHTML;
+	}
+	for (var element of document.getElementsByTagName("strong")) {
+		var replacementTag = document.createElement("strong")
+		var delay = 0;
+		for (var char of element.textContent) {
+			var newElement = document.createElement("span");
+			if (char == " ") {
+				newElement.innerHTML = "&nbsp;"
+			} else {
+				newElement.innerHTML = char;
+			}
+			newElement.style.animationDelay = delay + "s";
+			replacementTag.appendChild(newElement)
+			delay -= 0.1;
+		}
+		element.innerHTML = replacementTag.innerHTML;
+	}
+}
